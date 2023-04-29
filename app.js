ed481631b21bbe9556d84bb75d47d94ffcab0935
Andres <gallegosandres.tareas@gmail.com>
2023-04-28 20:29:33 -0500

    Se agraga la validacion de todos los campos, ademas de guardar los datos

--- app.js
+++ app.js
@@ -1,57 +1,3 @@
-function sayHello(){
-    let nombre = document.getElementById('nombre_completo_in')
-    console.log(nombre)
-    alert('Hola ' + nombre.value)
-}
-
-function getDataForm(){
-    
-    let email=document.getElementById('email_in')
-
-    if(email.value == ''){
-        alert('El campo email es obligatorio')
-        email.setAttribute('style', 'border-color:red')
-        email.setAttribute('placeholder','Escriba aqui el email')
-        return
-    }
-
-    let name=document.getElementById('nombre_completo_in')
-
-    if(name.value == ''){
-        alert('El campo nombre es obligatorio')
-        name.setAttribute('style', 'border-color:red')
-        name.setAttribute('placeholder','Escriba aqui el nombre')
-        return
-    }
-
-    let date_born=document.getElementById('fecha_na_in')
-
-    if(date_born.value == ''){
-        alert('El campo fecha es obligatorio')
-        date_born.setAttribute('style', 'border-color:red')
-        date_born.setAttribute('placeholder','Escriba aqui el fecha')
-        return
-    }
-
-    let sexo=document.getElementById('select_in')
-
-    if(sexo.value == ''){
-        alert('El campo sexo es obligatorio')
-        sexo.setAttribute('style', 'border-color:red')
-        sexo.setAttribute('placeholder','Escriba aqui el sexo')
-        return
-    }
-        
-    const datosFormulario={
-        name: name.value,
-        date_born: date_born.value,
-        email: email.value,
-        sexo: sexo.value
-    }
-
-    console.log(datosFormulario)
-}
-
 function validateState(element){
     if(element.value == ''){
         element.setAttribute('style','border-color:red')
@@ -60,19 +6,63 @@ function validateState(element){
     }
 }
 
+function validateInputs(id,msg){
+    const ob=document.getElementById(id)
+    if(ob.value == ''){
+        alert('Debe ingresar ' + msg)
+        ob.setAttribute('style','border-color:red')
+        return false
+    }
+    ob.removeAttribute('style')
+    return true
+}
+
 function validateInterests(){
     const Interests=document.querySelectorAll('#Interests')
-    
+    const cont=document.getElementById('id-cont-in')
     for(let i=0; i<Interests.length;i++){
         if(Interests[i].checked){
+            cont.removeAttribute('style')
             return true
         }
     }
+    cont.setAttribute('style','border:1px solid; border-color:red;')
     return false
 }
 
 function register(){
+    if(!validateInputs('email_in','un correo electronico')){return}
+    if(!validateInputs('nombre_completo_in','un nombre')){return}
+    if(!validateInputs('fecha_na_in','una fecha')){return}
+
     if (!validateInterests()){
         alert('Debe seleccionar por lo menos un interes')
+        return
     }
+
+    const request={
+        email: GetValue('email_in'),
+        name: GetValue('nombre_completo_in'),
+        date_born: GetValue('fecha_na_in'),
+        sexo: GetValue('select_in'),
+        interests: GetValueArray('Interests')
+    }
+
+    console.log(request)
+}
+
+function GetValue(id){
+    const ob=document.getElementById(id)
+    return ob.value
+}
+
+function GetValueArray(id){
+    const ob=document.querySelectorAll('#'+id)
+    let array=[]
+    for(let i=0;i<ob.length;i++){
+        if(ob[i].checked){
+            array.push(ob[i].value)
+        }
+    }
+    return array
 }
---
+++ styleexcard.js
@@ -0,0 +1,53 @@
+function sayHello(){
+    let nombre = document.getElementById('nombre_completo_in')
+    console.log(nombre)
+    alert('Hola ' + nombre.value)
+}
+
+function getDataForm(){
+    
+    let email=document.getElementById('email_in')
+
+    if(email.value == ''){
+        alert('El campo email es obligatorio')
+        email.setAttribute('style', 'border-color:red')
+        email.setAttribute('placeholder','Escriba aqui el email')
+        return
+    }
+
+    let name=document.getElementById('nombre_completo_in')
+
+    if(name.value == ''){
+        alert('El campo nombre es obligatorio')
+        name.setAttribute('style', 'border-color:red')
+        name.setAttribute('placeholder','Escriba aqui el nombre')
+        return
+    }
+
+    let date_born=document.getElementById('fecha_na_in')
+
+    if(date_born.value == ''){
+        alert('El campo fecha es obligatorio')
+        date_born.setAttribute('style', 'border-color:red')
+        date_born.setAttribute('placeholder','Escriba aqui el fecha')
+        return
+    }
+
+    let sexo=document.getElementById('select_in')
+
+    if(sexo.value == ''){
+        alert('El campo sexo es obligatorio')
+        sexo.setAttribute('style', 'border-color:red')
+        sexo.setAttribute('placeholder','Escriba aqui el sexo')
+        return
+    }
+        
+    const datosFormulario={
+        name: name.value,
+        date_born: date_born.value,
+        email: email.value,
+        sexo: sexo.value
+    }
+
+    console.log(datosFormulario)
+}
